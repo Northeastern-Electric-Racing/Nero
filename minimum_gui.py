d46f344131b57eee696de7120776c9108a76e284
HubotChen <98792428+HubotChen@users.noreply.github.com>
2023-01-10 20:45:56 -0500

    Create minimum_gui.py

---
+++ minimum_gui.py
@@ -0,0 +1,77 @@
+import tkinter
+import customtkinter
+
+import os
+import can
+import time
+
+from ner_processing.message import Message
+from ner_processing.master_mapping import MESSAGE_IDS
+from ner_processing.master_mapping import DATA_IDS
+
+os.system('sudo ip link set can0 type can bitrate 1000000')
+os.system('sudo ifconfig can0 up')
+
+can0 = can.interface.Bus(
+    channel='can0', bustype='socketcan')  # socketcan_native
+
+current_data = [None] * len(DATA_IDS)
+
+# Modes: "System" (standard), "Dark", "Light"
+customtkinter.set_appearance_mode("dark")
+# Themes: "blue" (standard), "green", "dark-blue"
+customtkinter.set_default_color_theme("./themes/ner.json")
+
+
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+
+        # configure window
+        self.title("NERO")
+        self.geometry(f"{1100}x{580}")
+
+        # create 2x2 grid system
+        self.grid_rowconfigure(1, weight=1)
+        self.grid_columnconfigure((0, 1), weight=1)
+
+        self.logo_label = customtkinter.CTkLabel(
+            master=self, text="NERO", font=customtkinter.CTkFont(size=36, weight="bold"), anchor="w")
+        self.logo_label.grid(row=0, column=0, sticky="w", padx=20, pady=10)
+
+        self.speed = customtkinter.CTkLabel(
+            master=self, text="Speed:", font=customtkinter.CTkFont(size=30, weight="bold"))
+        self.speed.grid(row=1, column=1)
+        self.mph = customtkinter.CTkLabel(
+            master=self, text="45 mph", font=customtkinter.CTkFont(size=36, weight="bold"))
+        
+        self.mph.grid(row=2, column=1)
+        self.mph.after(100, self.update_speed)
+        self.after(10, self.check_can)
+
+    def check_can(self):
+        msg = can0.recv(10.0)
+
+        # if msg.arbitration_id in MESSAGE_IDS:
+        if msg.arbitration_id == 165:
+            timestamp = int(float(msg.timestamp)*1000)
+            id = int(msg.arbitration_id)
+            length = int(msg.dlc)
+            data = [int(x) for x in msg.data]
+            msg = Message(timestamp, id, data)
+            decodedList = msg.decode()
+            for data in decodedList:
+                current_data[data.id] = data.value
+                print(str(data.id) +
+                      " (" + str(DATA_IDS[data.id]) + "): " + str(data.value))
+
+        if msg is None:
+            print('Timeout occurred, no message.')
+
+    def update_speed(self):
+        self.mph.configure(text=str(time.time()))
+        self.mph.after(100, self.update_speed)
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
