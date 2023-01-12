dacf3babeaf3927e4a0fdf1949903bc265cb3842
HubotChen <98792428+HubotChen@users.noreply.github.com>
2023-01-11 20:19:18 -0500

    just trying shit lmao

--- minimum_gui.py
+++ minimum_gui.py
@@ -9,6 +9,7 @@ from ner_processing.message import Message
 from ner_processing.master_mapping import MESSAGE_IDS
 from ner_processing.master_mapping import DATA_IDS
 
+os.system('sudo ifconfig can0 down')
 os.system('sudo ip link set can0 type can bitrate 1000000')
 os.system('sudo ifconfig can0 up')
 
@@ -74,24 +75,24 @@ class App(customtkinter.CTk):
         self.after(0, self.check_can)
 
     def update_speed(self):
-        # msg = can0.recv(10.0)
-        # # if msg.arbitration_id in MESSAGE_IDS:
-        # if msg.arbitration_id == 165:
-        #     timestamp = int(float(msg.timestamp)*1000)
-        #     id = int(msg.arbitration_id)
-        #     length = int(msg.dlc)
-        #     data = [int(x) for x in msg.data]
-        #     msg = Message(timestamp, id, data)
-        #     decodedList = msg.decode()
-        #     for data in decodedList:
-        #         current_data[data.id] = data.value
-        #         print(str(data.id) +
-        #               " (" + str(DATA_IDS[data.id]) + "): " + str(data.value))
+        msg = can0.recv(10.0)
+        # if msg.arbitration_id in MESSAGE_IDS:
+        if msg.arbitration_id == 165:
+            timestamp = int(float(msg.timestamp)*1000)
+            id = int(msg.arbitration_id)
+            length = int(msg.dlc)
+            data = [int(x) for x in msg.data]
+            msg = Message(timestamp, id, data)
+            decodedList = msg.decode()
+            for data in decodedList:
+                current_data[data.id] = data.value
+                print(str(data.id) +
+                      " (" + str(DATA_IDS[data.id]) + "): " + str(data.value))
 
-        # if msg is None:
-        #     print('Timeout occurred, no message.')
+        if msg is None:
+            print('Timeout occurred, no message.')
 
-        self.mph.configure(text=str(time.time()))
+        self.mph.configure(text=str(current_data[45]))
         self.mph.after(1, self.update_speed)
 
     def button_callback(self):
