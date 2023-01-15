ebf8ea160163bfa672e0105565f7a16571d1e592
HubotChen <98792428+HubotChen@users.noreply.github.com>
2023-01-15 12:36:07 -0500

    bug fix to prevent update if nonetype

--- minimum_gui.py
+++ minimum_gui.py
@@ -89,7 +89,8 @@ class App(customtkinter.CTk):
         self.after(1, self.check_can)
 
     def update_speed(self):
-        self.mph.configure(text=str(float(current_data[45]) * 0.01272))
+        if not isinstance(current_data[45], None):
+            self.mph.configure(text=str(float(current_data[45]) * 0.01272))
         self.mph.after(100, self.update_speed)
 
     def button_callback(self):
