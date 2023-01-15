1ae938bc2fbe95aa4e8628dd5733beb6c7e4c386
HubotChen <98792428+HubotChen@users.noreply.github.com>
2023-01-15 12:26:23 -0500

    bug fix
    bug fix

--- minimum_gui.py
+++ minimum_gui.py
@@ -89,7 +89,7 @@ class App(customtkinter.CTk):
         self.after(1, self.check_can)
 
     def update_speed(self):
-        self.mph.configure(text=str(current_data[45] * 0.01272))
+        self.mph.configure(text=str(float(current_data[45]) * 0.01272))
         self.mph.after(100, self.update_speed)
 
     def button_callback(self):
