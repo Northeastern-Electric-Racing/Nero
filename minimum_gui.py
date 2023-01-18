f25afda99df38c2d62dd3ebc13f2e3dd119bfb2f
Matthew McCauley <60201095+mccauleyma@users.noreply.github.com>
2023-01-18 17:31:53 -0500

    Minimum viable product (#4)
    * Create minimum_gui.py
    
    * fixed bug
    
    * fixed bug
    
    * fixed bug
    
    * fixed bug
    
    * fixed bug
    
    * fixed bug
    
    * fixed bug
    
    * fixed bug
    
    * fixed bug
    
    * just trying shit lmao
    
    * fixed bug
    
    * just trying shit lmao
    
    * fixed bug
    
    * just trying shit lmao
    
    * fixed bug
    
    * fixed bug
    
    * fixed bug
    
    * fixed bug
    
    * fixed bug
    
    * fixed bug
    
    * updated to do speed
    
    updated to do speed
    
    * bug fix
    
    bug fix
    
    * bug fix
    
    bug fix
    
    * bug fix to prevent update if nonetype
    
    * bug fix
    
    * bug fix
    
    * bug fix?
    
    * fixed round to int
    
    * adding basic statuses
    
    * Current data
    
    * Added LV batt voltage
    
    Co-authored-by: HubotChen <98792428+HubotChen@users.noreply.github.com>
    Co-authored-by: Nick DePatie <hubert>

---
+++ minimum_gui.py
@@ -0,0 +1,150 @@
+from ner_processing.master_mapping import DATA_IDS
+from ner_processing.master_mapping import MESSAGE_IDS
+from ner_processing.message import Message
+import time
+import can
+import customtkinter
+import tkinter
+import os
+import sys
+
+os.system('echo $PWD')
+os.chdir("/home/ner/Desktop/Nero/")
+os.system("echo $PWD")
+# /home/ner/Desktop/Nero/
+
+# sudo python3 /home/ner/Desktop/Nero/minimum_gui.py
+
+
+os.environ.__setitem__('DISPLAY', ':0.0')
+
+os.system('sudo ifconfig can0 down')
+os.system('sudo ip link set can0 type can bitrate 1000000')
+os.system('sudo ifconfig can0 up')
+
+
+can0 = can.interface.Bus(
+    channel='can0', bustype='socketcan')  # socketcan_native
+
+current_data = [None] * len(DATA_IDS)
+
+# Modes: "System" (standard), "Dark", "Light"
+customtkinter.set_appearance_mode("dark")
+# Themes: "blue" (standard), "green", "dark-blue"
+customtkinter.set_default_color_theme("/home/ner/Desktop/Nero/themes/ner.json")
+
+
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+
+        # configure window
+        self.title("NERO")
+        self.geometry(f"{1100}x{580}")
+
+        # create 2x2 grid system
+        self.grid_rowconfigure(1, weight=1)
+        self.grid_columnconfigure((0, 1), weight=1)
+
+        self.logo_label = customtkinter.CTkLabel(
+            master=self, text="NERO", font=customtkinter.CTkFont(size=36, weight="bold"), anchor="w")
+        self.logo_label.grid(row=0, column=0, sticky="w", padx=20, pady=10)
+
+        self.speed = customtkinter.CTkLabel(
+            master=self, text="Speed:", font=customtkinter.CTkFont(size=30, weight="bold"))
+        self.speed.grid(row=0, column=2)
+        self.mph = customtkinter.CTkLabel(
+            master=self, text="69 mph", font=customtkinter.CTkFont(size=36, weight="bold"))
+        self.mph.grid(row=1, column=2)
+
+        self.status = customtkinter.CTkLabel(
+            master=self, text="Status:", font=customtkinter.CTkFont(size=30, weight="bold"))
+        self.status.grid(row=1, column=0)
+        self.statusD = customtkinter.CTkLabel(
+            master=self, text="NO DATA", font=customtkinter.CTkFont(size=36, weight="bold"))
+        self.statusD.grid(row=1, column=1)
+
+        self.dir = customtkinter.CTkLabel(
+            master=self, text="Direction:", font=customtkinter.CTkFont(size=30, weight="bold"))
+        self.dir.grid(row=2, column=0)
+        self.dirD = customtkinter.CTkLabel(
+            master=self, text="NO DATA", font=customtkinter.CTkFont(size=36, weight="bold"))
+        self.dirD.grid(row=2, column=1)
+
+        self.curr = customtkinter.CTkLabel(
+            master=self, text="Current:", font=customtkinter.CTkFont(size=30, weight="bold"))
+        self.curr.grid(row=3, column=0)
+        self.currD = customtkinter.CTkLabel(
+            master=self, text="NO DATA", font=customtkinter.CTkFont(size=36, weight="bold"))
+        self.currD.grid(row=3, column=1)
+
+        self.LVBatt = customtkinter.CTkLabel(
+            master=self, text="LV Batt Volt:", font=customtkinter.CTkFont(size=30, weight="bold"))
+        self.LVBatt.grid(row=4, column=0)
+        self.LVBattD = customtkinter.CTkLabel(
+            master=self, text="NO DATA", font=customtkinter.CTkFont(size=36, weight="bold"))
+        self.LVBattD.grid(row=4, column=1)
+
+        self.check_can()
+        self.update_speed()
+        self.update_status()
+        self.update_dir()
+        self.update_curr()
+        self.update_LVBatt()
+
+    def check_can(self):
+        msg = can0.recv(10.0)
+
+        if msg.arbitration_id in MESSAGE_IDS:
+            # if msg.arbitration_id == 165:
+            timestamp = int(float(msg.timestamp)*1000)
+            id = int(msg.arbitration_id)
+            length = int(msg.dlc)
+            data = [int(x) for x in msg.data]
+            msg = Message(timestamp, id, data)
+            decodedList = msg.decode()
+            for data in decodedList:
+                current_data[data.id] = data.value
+                print(str(data.id) +
+                      " (" + str(DATA_IDS[data.id]) + "): " + str(data.value))
+
+        if msg is None:
+            print('Timeout occurred, no message.')
+
+        self.after(1, self.check_can)
+
+    def update_speed(self):
+        if current_data[45] is not None:
+            self.mph.configure(text=str(round(current_data[45] * 0.01272)))
+        self.mph.after(100, self.update_speed)
+
+    def update_status(self):
+        if current_data[85] is not None:
+            if current_data[85] == 1:
+                self.statusD.configure(text="ON")
+            else:
+                self.statusD.configure(text="OFF")
+        self.statusD.after(100, self.update_status)
+
+    def update_dir(self):
+        if current_data[84] is not None:
+            if current_data[84] == 1:
+                self.dirD.configure(text="FORWARD")
+            else:
+                self.dirD.configure(text="REVERSE")
+        self.dirD.after(100, self.update_dir)
+
+    def update_curr(self):
+        if current_data[2] is not None:
+            self.currD.configure(text=str(current_data[2]))
+        self.currD.after(100, self.update_curr)
+
+    def update_LVBatt(self):
+        if current_data[63] is not None:
+            self.LVBattD.configure(text=str(current_data[63]))
+        self.LVBattD.after(100, self.update_LVBatt)
+
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
