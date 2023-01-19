742999ac1837cb91f846b2c623a606f9dd4e2238
Peyton-McKee <mckee.p@northeastern.edu>
2023-01-19 10:29:59 -0500

    #6 Copy over work from MacOS branch
    specifies frame from tkinter due to error where tkinter also has a class called Message that conflicted with the Message class from ner_processing.
    
    Refactored the frames' details to instead display the information determined prior by James and Matthew.
    
    Created update functions for when can gets involved
    
    Added a check that determines whether or not its on linux to see whether or not to use can, otherwise can would throw errors
    
    created a basic frontend system aligned with what James outlined
    
    made default values "NO DATA" as requested by matthew
    
    we should plan to refactor functions to another class/ discuss abstracting the functions if possible

--- minimum_gui.py
+++ minimum_gui.py
@@ -4,34 +4,34 @@ from ner_processing.message import Message
 import time
 import can
 import customtkinter
-import tkinter
+from tkinter import Frame
 import os
 import sys
+import platform
 
-os.system('echo $PWD')
-os.chdir("/home/ner/Desktop/Nero/")
-os.system("echo $PWD")
-# /home/ner/Desktop/Nero/
+if (platform.platform()[0:5] == "Linux"):
+    os.system('echo $PWD')
+    os.chdir("/home/ner/Desktop/Nero/")
+    os.system("echo $PWD")
+    # /home/ner/Desktop/Nero/
 
-# sudo python3 /home/ner/Desktop/Nero/minimum_gui.py
+    # sudo python3 /home/ner/Desktop/Nero/minimum_gui.py
 
+    os.environ.__setitem__('DISPLAY', ':0.0')
 
-os.environ.__setitem__('DISPLAY', ':0.0')
+    os.system('sudo ifconfig can0 down')
+    os.system('sudo ip link set can0 type can bitrate 1000000')
+    os.system('sudo ifconfig can0 up')
 
-os.system('sudo ifconfig can0 down')
-os.system('sudo ip link set can0 type can bitrate 1000000')
-os.system('sudo ifconfig can0 up')
-
-
-can0 = can.interface.Bus(
-    channel='can0', bustype='socketcan')  # socketcan_native
+    can0 = can.interface.Bus(
+        channel='can0', bustype='socketcan')  # socketcan_native
 
 current_data = [None] * len(DATA_IDS)
 
 # Modes: "System" (standard), "Dark", "Light"
 customtkinter.set_appearance_mode("dark")
 # Themes: "blue" (standard), "green", "dark-blue"
-customtkinter.set_default_color_theme("/home/ner/Desktop/Nero/themes/ner.json")
+customtkinter.set_default_color_theme("themes/ner.json")
 
 
 class App(customtkinter.CTk):
@@ -42,55 +42,134 @@ class App(customtkinter.CTk):
         self.title("NERO")
         self.geometry(f"{1100}x{580}")
 
-        # create 2x2 grid system
+        # configure the grid
         self.grid_rowconfigure(1, weight=1)
-        self.grid_columnconfigure((0, 1), weight=1)
+        self.grid_columnconfigure(0, weight=1)
 
-        self.logo_label = customtkinter.CTkLabel(
-            master=self, text="NERO", font=customtkinter.CTkFont(size=36, weight="bold"), anchor="w")
-        self.logo_label.grid(row=0, column=0, sticky="w", padx=20, pady=10)
+        # create top and bottom frames
+        self.top_frame = Frame(width=1100, height=300)
+        self.bottom_frame = Frame(width=1100, height=300)
 
-        self.speed = customtkinter.CTkLabel(
-            master=self, text="Speed:", font=customtkinter.CTkFont(size=30, weight="bold"))
-        self.speed.grid(row=0, column=2)
+        self.top_frame.grid(row=0, column=0)
+        self.bottom_frame.grid(row=1, column=0)
+
+        # Configure grids for top and bottom frames
+        self.top_frame.grid_rowconfigure(0, weight=1)
+        self.top_frame.grid_columnconfigure(1, weight=1)
+
+        self.bottom_frame.grid_rowconfigure(0, weight=1)
+        self.bottom_frame.grid_columnconfigure(1, weight=1)
+
+        # create the top two frames
+        self.top_right_frame = Frame(self.top_frame, width=550, height=300,
+                                     bg="black", highlightbackground="gray", highlightthickness=1)
+        self.top_left_frame = Frame(self.top_frame, width=550, height=300,
+                                    bg="black", highlightbackground="gray", highlightthickness=1)
+
+        self.top_right_frame.grid(row=0, column=1)
+        self.top_left_frame.grid(row=0, column=0)
+
+        self.top_left_frame.grid_propagate(False)
+        self.top_left_frame.grid_rowconfigure(1, weight=1)
+        self.top_left_frame.grid_columnconfigure(0, weight=1)
+
+        self.top_right_frame.grid_propagate(False)
+        self.top_right_frame.grid_rowconfigure(1, weight=1)
+        self.top_right_frame.grid_columnconfigure(0, weight=1)
+
+        # create the bottom three frames
+        self.bottom_right_frame = Frame(
+            self.bottom_frame, width=367, height=300, bg="black", highlightbackground="gray", highlightthickness=1)
+        self.bottom_left_frame = Frame(self.bottom_frame, width=367, height=300,
+                                       bg="black", highlightbackground="gray", highlightthickness=1)
+        self.bottom_middle_frame = Frame(
+            self.bottom_frame, width=366, height=300, bg="black", highlightbackground="gray", highlightthickness=1)
+
+        self.bottom_right_frame.grid(row=0, column=2)
+        self.bottom_left_frame.grid(row=0, column=0)
+        self.bottom_middle_frame.grid(row=0, column=1)
+
+        self.bottom_left_frame.grid_propagate(False)
+        self.bottom_left_frame.grid_rowconfigure(1, weight=1)
+        self.bottom_left_frame.grid_columnconfigure(0, weight=1)
+
+        self.bottom_middle_frame.grid_propagate(False)
+        self.bottom_middle_frame.grid_rowconfigure(1, weight=1)
+        self.bottom_middle_frame.grid_columnconfigure(0, weight=1)
+
+        self.bottom_right_frame.grid_propagate(False)
+        self.bottom_right_frame.grid_rowconfigure(1, weight=1)
+        self.bottom_right_frame.grid_columnconfigure(0, weight=1)
+
+        # create top left frame
+        self.mph_frame = Frame(self.top_left_frame,
+                               width=550, height=150, bg="black")
         self.mph = customtkinter.CTkLabel(
-            master=self, text="69 mph", font=customtkinter.CTkFont(size=36, weight="bold"))
-        self.mph.grid(row=1, column=2)
+            master=self.mph_frame, text="NO DATA", font=customtkinter.CTkFont(size=150, weight="bold"))
 
+        self.mph_label = customtkinter.CTkLabel(
+            master=self.mph_frame, text="mph", font=customtkinter.CTkFont(size=20))
+
+        self.mph_frame.grid(row=0, column=0, sticky="s")
+        self.mph.grid(row=0, column=0)
+        self.mph_label.grid(row=0, column=1, sticky="s")
+
+        self.km_frame = Frame(self.top_left_frame,
+                              width=550, height=150, bg="black")
+        self.km = customtkinter.CTkLabel(
+            master=self.km_frame, text=self.calculate_kph(), font=customtkinter.CTkFont(size=25))
+        self.km_label = customtkinter.CTkLabel(
+            master=self.km_frame, text=" kmph", font=customtkinter.CTkFont(size=25))
+
+        self.km_frame.grid(row=1, column=0, sticky="n")
+        self.km.grid(row=0, column=0)
+        self.km_label.grid(row=0, column=1)
+
+        # create top right frame
         self.status = customtkinter.CTkLabel(
-            master=self, text="Status:", font=customtkinter.CTkFont(size=30, weight="bold"))
-        self.status.grid(row=1, column=0)
-        self.statusD = customtkinter.CTkLabel(
-            master=self, text="NO DATA", font=customtkinter.CTkFont(size=36, weight="bold"))
-        self.statusD.grid(row=1, column=1)
+            master=self.top_right_frame, text="NO DATA", font=customtkinter.CTkFont(size=100, weight="bold"))
 
         self.dir = customtkinter.CTkLabel(
-            master=self, text="Direction:", font=customtkinter.CTkFont(size=30, weight="bold"))
-        self.dir.grid(row=2, column=0)
-        self.dirD = customtkinter.CTkLabel(
-            master=self, text="NO DATA", font=customtkinter.CTkFont(size=36, weight="bold"))
-        self.dirD.grid(row=2, column=1)
+            master=self.top_right_frame, text="NO DATA", font=customtkinter.CTkFont(size=100, weight="bold"))
 
-        self.curr = customtkinter.CTkLabel(
-            master=self, text="Current:", font=customtkinter.CTkFont(size=30, weight="bold"))
-        self.curr.grid(row=3, column=0)
-        self.currD = customtkinter.CTkLabel(
-            master=self, text="NO DATA", font=customtkinter.CTkFont(size=36, weight="bold"))
-        self.currD.grid(row=3, column=1)
+        self.status.grid(row=0, column=0, sticky="s")
+        self.dir.grid(row=1, column=0, sticky="n")
 
-        self.LVBatt = customtkinter.CTkLabel(
-            master=self, text="LV Batt Volt:", font=customtkinter.CTkFont(size=30, weight="bold"))
-        self.LVBatt.grid(row=4, column=0)
-        self.LVBattD = customtkinter.CTkLabel(
-            master=self, text="NO DATA", font=customtkinter.CTkFont(size=36, weight="bold"))
-        self.LVBattD.grid(row=4, column=1)
+        # create bottom left frame
+        self.pack_temp = customtkinter.CTkLabel(
+            master=self.bottom_left_frame, text="NO DATA", font=customtkinter.CTkFont(size=150, weight="bold"))
+        self.pack_temp_label = customtkinter.CTkLabel(
+            master=self.bottom_left_frame, text="Pack Temperature", font=customtkinter.CTkFont(size=20))
 
-        self.check_can()
-        self.update_speed()
-        self.update_status()
-        self.update_dir()
-        self.update_curr()
-        self.update_LVBatt()
+        self.pack_temp.grid(row=0, column=0, sticky="s")
+        self.pack_temp_label.grid(row=1, column=0, sticky="n")
+
+        # create bottom middle frame
+        self.motor_temp = customtkinter.CTkLabel(
+            master=self.bottom_middle_frame, text="NO DATA", font=customtkinter.CTkFont(size=150, weight="bold"))
+        self.motor_temp_label = customtkinter.CTkLabel(
+            master=self.bottom_middle_frame, text="Motor Temperature", font=customtkinter.CTkFont(size=20))
+
+        self.motor_temp.grid(row=0, column=0, sticky="s")
+        self.motor_temp_label.grid(row=1, column=0, sticky="n")
+
+        # create bottom right frame
+        self.state_charge = customtkinter.CTkLabel(
+            master=self.bottom_right_frame, text="NO DATA", font=customtkinter.CTkFont(size=150, weight="bold"))
+        self.state_charge_label = customtkinter.CTkLabel(
+            master=self.bottom_right_frame, text="State of Charge", font=customtkinter.CTkFont(size=20))
+
+        self.state_charge.grid(row=0, column=0, sticky="s")
+        self.state_charge_label.grid(row=1, column=0, sticky="n")
+
+        if (platform.platform()[0:5] == "Linux"):
+            self.check_can()
+            self.update_speed()
+            self.update_status()
+            self.update_dir()
+            self.update_motor_temp()
+            self.update_pack_temp()
+            self.update_state_charge()
 
     def check_can(self):
         msg = can0.recv(10.0)
@@ -113,36 +192,57 @@ class App(customtkinter.CTk):
 
         self.after(1, self.check_can)
 
+    def calculate_kph(self):
+        if self.mph._text != "NO DATA":
+            return int(int(self.mph._text) * 1.60934)
+        return "NO DATA"
+
     def update_speed(self):
         if current_data[45] is not None:
             self.mph.configure(text=str(round(current_data[45] * 0.01272)))
+            self.km.configure(text=str(self.calculate_kph()))
         self.mph.after(100, self.update_speed)
 
     def update_status(self):
         if current_data[85] is not None:
             if current_data[85] == 1:
-                self.statusD.configure(text="ON")
+                self.status.configure(text="ON", text_color="green")
             else:
-                self.statusD.configure(text="OFF")
-        self.statusD.after(100, self.update_status)
+                self.status.configure(text="OFF", text_color="red")
+        self.status.after(100, self.update_status)
 
     def update_dir(self):
         if current_data[84] is not None:
             if current_data[84] == 1:
-                self.dirD.configure(text="FORWARD")
+                self.dir.configure(text="FORWARD")
             else:
-                self.dirD.configure(text="REVERSE")
-        self.dirD.after(100, self.update_dir)
+                self.dir.configure(text="REVERSE")
+        self.dir.after(100, self.update_dir)
+
+    def update_pack_temp(self):
+        if current_data[10] is not None:
+            self.pack_temp.configure(text=str(current_data[4]) + "°")
+        self.pack_temp.after(100, self.update_pack_temp)
+
+    def update_motor_temp(self):
+        if current_data[28] is not None:
+            self.motor_temp.configure(text=str(current_data[5]) + "°")
+        self.motor_temp.after(100, self.update_motor_temp)
+
+    def update_state_charge(self):
+        if current_data[4] is not None:
+            self.state_charge.configure(text=str(current_data[6]) + "%")
+        self.state_charge.after(100, self.update_state_charge)
 
     def update_curr(self):
         if current_data[2] is not None:
-            self.currD.configure(text=str(current_data[2]))
-        self.currD.after(100, self.update_curr)
+            self.current.configure(text=str(current_data[2]))
+        self.current.after(100, self.update_curr)
 
     def update_LVBatt(self):
         if current_data[63] is not None:
-            self.LVBattD.configure(text=str(current_data[63]))
-        self.LVBattD.after(100, self.update_LVBatt)
+            self.LVBatt.configure(text=str(current_data[63]))
+        self.LVBatt.after(100, self.update_LVBatt)
 
 
 if __name__ == "__main__":
