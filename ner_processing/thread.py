7877d76b2f8d1c2020ab826f909b865771dc938e
AC-Dc <changdavidson4@gmail.com>
2023-02-18 21:32:49 -0500

    remove ner_processing directory & from git, add submodule

--- ner_processing/thread.py
+++
@@ -1,9 +0,0 @@
-from ner_processing.decode_files import LogFormat, processLine
-from ner_processing.message import Message
-
-FORMAT = LogFormat.TEXTUAL1
-
-
-def thread(line):
-    message: Message = processLine(line, FORMAT)
-    return message.decode()
