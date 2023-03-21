51edeece64110dba440561f40ebaef1d55778523
Peyton-McKee <mckee.p@northeastern.edu>
2023-03-21 00:26:11 -0400

    #26 I think i figured it out

--- modes/debug_mode/debug_table_page.py
+++ modes/debug_mode/debug_table_page.py
@@ -179,8 +179,7 @@ class DebugTable(Page):
         self.highlightItem()
         # Determines if the table should reload to the prior table
         if self.selected_id == 0:
-            new_base_id = int((len(self.table) - 1) / 2) - self.max_row_count if int((len(self.table) - 1) / 2) - self.max_row_count > 0 else 0
-            new_base_id = new_base_id + 1 if len(self.table) - 1 % 2 == 1 else new_base_id
+            new_base_id = int((len(self.table)) / 2) - self.max_row_count + 1 if int((len(self.table)) / 2) - self.max_row_count + 1 > 0 else 0
             self.selected_id = len(self.table) - 1
             self.create_table(new_base_id)
         elif self.selected_id % self.max_row_count == 0:
