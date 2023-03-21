7dc2f5536122335821f6da3a1180e862861127d8
Peyton-McKee <mckee.p@northeastern.edu>
2023-03-21 01:51:34 -0400

    #26 OK probably this

--- modes/debug_mode/debug_table_page.py
+++ modes/debug_mode/debug_table_page.py
@@ -183,7 +183,7 @@ class DebugTable(Page):
             self.selected_id = len(self.table) - 1
             self.create_table(new_base_id)
         elif int(self.selected_id / 2) % (self.max_row_count - 1) == 0:
-            self.create_table(int(self.selected_id / 2 - self.max_row_count -  1))
+            self.create_table(int(self.selected_id / 2) - (self.max_row_count - 1))
             self.selected_id -= 1
         else:
             self.selected_id -= 1
@@ -196,7 +196,7 @@ class DebugTable(Page):
         if len(self.table) - 1 == self.selected_id:
             self.create_table(0)
             self.selected_id = 0
-        elif int(self.selected_id / 2) % (self.max_row_count - 1) == self.max_row_count - 2:
+        elif int(self.selected_id / 2) % (self.max_row_count - 1) == self.max_row_count - 2 and self.selected_id % 2 == 1:
             self.create_table(int(self.selected_id / 2) + 1)
             self.selected_id += 1
         else:
