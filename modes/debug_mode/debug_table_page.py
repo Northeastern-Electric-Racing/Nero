2424bf2c2b684ea8b33a23a868acfa8cf620a9ab
Peyton-McKee <mckee.p@northeastern.edu>
2023-03-23 23:37:10 -0400

    #26 revert

--- modes/debug_mode/debug_table_page.py
+++ modes/debug_mode/debug_table_page.py
@@ -213,8 +213,6 @@ class DebugTable(Page):
 
     def update_by_id(self, id: int):
         generic_text = self.model.get_by_id(id)
-        if id == 107:
-            generic_text = bin(generic_text)[2:][::-1]
         self.table[id].value_label.configure(text=generic_text)
 
     def update(self):
