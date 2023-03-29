4db385ab67d39a5a194de96a674c08c0d1e006bf
Peyton-McKee <mckee.p@northeastern.edu>
2023-03-29 02:25:24 -0400

    #36 Remove unused parts of Function

--- components/battery_progress.py
+++ components/battery_progress.py
@@ -3,17 +3,13 @@ from tkinter import *
 
 
 class BatteryProgress(Canvas):
-    def __init__(self, parent, x0: int, y0: int, x1: int, y1: int, width=2, full_extent=100):
+    def __init__(self, parent, x0: int, y0: int, x1: int, y1: int):
         super().__init__(parent, background="black", highlightthickness=0, width=0, height=0)
         self.custom_font = tkFont.Font(family="Helvetica", size=int(((x1 - x0) / 2) - 5), weight='bold')
-        self.x0, self.y0, self.x1, self.y1 = x0+width, y0+width, x1-width, y1-width
+        self.x0, self.y0, self.x1, self.y1 = x0, y0, x1, y1
         self.tx, self.ty = (x1+x0) / 2, (y1+y0) / 2
-        self.width = width
-        self.full_extent = full_extent
-        # draw static bar outline
-        w2 = width / 2
-        self.extent = 0
 
+        self.extent = 0
         self.rectangle_1 = self.create_rectangle(self.x0, self.y0 ,
                                          self.x1, self.y1, outline="white")
         self.rectangle_top = self.create_rectangle(self.tx - (self.tx - x0) /2, self.y0-self.ty/20,
--- modes/efficiency_mode/efficiency_mode.py
+++ modes/efficiency_mode/efficiency_mode.py
@@ -1,9 +1,8 @@
 from modes.mode import Mode
 from tkinter import Frame
-from modes.efficiency_mode.efficiency_page import Efficiency
 from models.model import Model
-
+from modes.home import Home
 
 class EfficiencyMode(Mode):
     def __init__(self, parent: Frame, controller: Frame, model: Model):
-        super().__init__(parent, controller, model, "Efficiency Mode", [Efficiency])
+        super().__init__(parent, controller, model, "Efficiency Mode", [Home])
