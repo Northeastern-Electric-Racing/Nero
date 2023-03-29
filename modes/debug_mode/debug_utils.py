9ded34a56eaef91eaada390ae70090d965d17df9
Peyton-McKee <mckee.p@northeastern.edu>
2023-03-28 22:03:35 -0400

    Revert "#39 array"
    This reverts commit 896441eda5cb844e4f5b2b1f33dfbec084c49a55.

--- modes/debug_mode/debug_utils.py
+++ modes/debug_mode/debug_utils.py
@@ -1,5 +1,6 @@
 from typing import List
-from constants import FAULTS
+from utils.fault_statuses import Fault_Statuses
+
 class DebugPlotValue:
     def __init__(self, name: str, unit: str, data: List[float]):
         self.data = data
@@ -12,7 +13,7 @@ class FaultInstance:
         fault_bin = bin(fault_decimal)[2:]
         for i in range(0, len(fault_bin)):
             if fault_bin[i] == '1':
-                self.faults.append(FAULTS[i])
+                self.faults.append(Fault_Statuses[i].name)
         self.max_cell_temp = max_cell_temp if max_cell_temp is not None else "N/A"
         self.max_cell_voltage = max_cell_voltage if max_cell_voltage is not None else "N/A"
         self.average_cell_temp = average_cell_temp if average_cell_temp is not None else "N/A"
